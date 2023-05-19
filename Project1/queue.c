73eb46d954057e8d03dc6bafb1f52668dba98aee
Badak <dltmdwls122789@gmail.com>
2023-05-19 20:47:12 +0900

    queue implemented

---
+++ Project1/queue.c
@@ -0,0 +1,69 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct queueNode {
+	int data;
+	struct queueNode* next;
+}qNode;
+
+typedef struct queue {
+	qNode* front;
+	qNode* rear;
+}queue;
+
+queue newQueue() {
+	queue tmp;
+	tmp.front = NULL;
+	tmp.rear = NULL;
+	
+	return tmp;
+}
+
+qNode* get_qNode() {
+	qNode* tmp = (qNode*)malloc(sizeof(qNode));
+	tmp->data = 0;
+	tmp->next = NULL;
+}
+
+void enqueue(queue* head, int data) {
+	qNode* tmp = get_qNode();
+	tmp->data = data;
+	
+	if (head->front == NULL) {
+		head->front = tmp;
+		head->rear = tmp;
+		return;
+	}
+
+	head->rear->next = tmp;
+
+	head->rear = tmp;
+}
+
+void dequeue(queue* head) {
+	qNode* tmp = head->front;
+
+	if (head->front == head->rear) head->rear = NULL;
+
+	head->front = head->front->next;
+
+	free(tmp);
+}
+
+void freeQueue(queue* head) {
+	while (head->front != NULL) {
+		dequeue(head);
+	}
+}
+
+int main() {
+	queue test = newQueue();
+
+	for (int i = 0; i < 40; i++) {
+		enqueue(&test, i);
+	}
+	printf("\n");
+
+	freeQueue(&test);
+}
